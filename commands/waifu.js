51bf31180ca212d4c84fb989c0f0f1895c5286e2
fjrhub <muhammadfajrulfalah@proton.me>
2025-11-02 15:51:45 +0700

    refactor(waifu): replace with conventional methods

--- commands/waifu.js
+++ commands/waifu.js
@@ -5,10 +5,9 @@ module.exports = {
   description: "Get a random waifu image from the API WaifuPics",
   async execute(ctx) {
     try {
-      const response = await axios.get(
-        createUrl("waifupics", "/sfw/waifu"),
-        { timeout: 8000 }
-      );
+      const response = await axios.get("https://api.waifu.pics/sfw/waifu", {
+        timeout: 8000,
+      });
       const imageUrl = response.data?.url;
 
       if (!imageUrl) {
