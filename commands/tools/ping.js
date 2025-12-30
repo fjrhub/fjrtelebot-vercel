ddbe6cd891d362c193759c9362b6ca672a79e161
fjrhub <muhammadfajrulfalah@proton.me>
2025-12-30 22:04:37 +0700

    feat: add validator ownerId

--- commands/tools/ping.js
+++ commands/tools/ping.js
@@ -1,6 +1,23 @@
+const OWNER_ID = Number(process.env.OWNER_ID);
+
+if (Number.isNaN(OWNER_ID)) {
+  throw new Error("OWNER_ID di .env bukan angka atau tidak terbaca");
+}
+
+export const isOwner = (ctx) => {
+  const id =
+    ctx.from?.id ||
+    ctx.callbackQuery?.from?.id ||
+    ctx.chat?.id;
+
+  console.log("VALIDATE ID:", id, "OWNER:", OWNER_ID);
+
+  return Number(id) === OWNER_ID;
+};
 export default {
   name: "ping",
   async execute(ctx) {
+    if (!isOwner(ctx)) return;
     await ctx.reply("🤖 pong!");
   },
 };
