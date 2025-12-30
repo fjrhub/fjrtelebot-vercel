18d03cb9fc12e3cf6a344f2c078ac57b916dab66
fjrhub <muhammadfajrulfalah@proton.me>
2025-12-30 22:07:56 +0700

    fix: error ping  validator

--- commands/tools/ping.js
+++ commands/tools/ping.js
@@ -1,23 +1,14 @@
-const OWNER_ID = Number(process.env.OWNER_ID);
-
-if (Number.isNaN(OWNER_ID)) {
-  throw new Error("OWNER_ID di .env bukan angka atau tidak terbaca");
-}
-
-export const isOwner = (ctx) => {
-  const id =
-    ctx.from?.id ||
-    ctx.callbackQuery?.from?.id ||
-    ctx.chat?.id;
-
-  console.log("VALIDATE ID:", id, "OWNER:", OWNER_ID);
-
-  return Number(id) === OWNER_ID;
-};
 export default {
   name: "ping",
   async execute(ctx) {
-    if (!isOwner(ctx)) return;
-    await ctx.reply("🤖 pong!");
+    const OWNER_ID = Number(process.env.OWNER_ID);
+
+    // validasi env
+    if (Number.isNaN(OWNER_ID)) return;
+
+    // validasi owner
+    if (ctx.from?.id !== OWNER_ID) return;
+
+    await ctx.reply(String(ctx.from.id));
   },
 };
