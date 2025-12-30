ddac61316d3ffa1cbf8a862d11361c37e92fdd1c
fjrhub <muhammadfajrulfalah@proton.me>
2025-12-30 22:09:27 +0700

    feat(ping): add owner validation using env variable

--- commands/tools/ping.js
+++ commands/tools/ping.js
@@ -1,14 +1,7 @@
 export default {
   name: "ping",
   async execute(ctx) {
-    const OWNER_ID = Number(process.env.OWNER_ID);
-
-    // validasi env
-    if (Number.isNaN(OWNER_ID)) return;
-
-    // validasi owner
-    if (ctx.from?.id !== OWNER_ID) return;
-
-    await ctx.reply(String(ctx.from.id));
+    if (ctx.from?.id !== Number(process.env.OWNER_ID)) return;
+    await ctx.reply(String("Pong!"));
   },
 };
