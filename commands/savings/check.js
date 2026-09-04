1497fc3ecd83e53d3446c7ad79a8f98d22d12f10
fjrhub <121226701+fjrhub@users.noreply.github.com>
2026-09-04 23:54:40 +0700

    feat: add jago to supported wallets

--- commands/savings/check.js
+++ commands/savings/check.js
@@ -4,7 +4,7 @@ import { InputFile } from "grammy";
 /* =========================
    DAFTAR AKUN (EDIT DI SINI SAJA)
 ========================= */
-const ALL_WALLETS = ["wallet", "dana", "seabank", "fjlsaldo", "gopay", "bank"];
+const ALL_WALLETS = ["wallet", "dana", "seabank", "fjlsaldo", "gopay", "bank", "jago"];
 // Tambah akun cukup:
 // const ALL_WALLETS = ["wallet", "dana", "seabank", "gopay"];
 
